4df77cebec755cae416205683c2e908e8c2fdf4c
Andrew Wong <coding.monkey.andrew@gmail.com>
2015-03-05 15:36:43 -1000

    add pathing for server views folder

--- app/server.js
+++ app/server.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var app = express();
+var path = require('path');
 
+app.set('views', path.join(__dirname, 'views'));
 
 app.set('view engine', 'jade');
 app.get('/', function (req, res) {
