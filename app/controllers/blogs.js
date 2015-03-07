72eceb5a2d77faff84bb91812cbc4024bb26fdd7
Andrew Wong <coding.monkey.andrew@gmail.com>
2015-03-06 19:09:29 -1000

    implement crude login

--- app/controllers/blogs.js
+++ app/controllers/blogs.js
@@ -2,31 +2,59 @@ var mongoose = require('mongoose');
 var express = require('express');
 var router = express.Router();
 var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
 
 var Blog = require('../models/Blog.js');
-var auth = require('./auth');
 
 mongoose.connect('mongodb://devleague:devleague-user@ds049661.mongolab.com:49661/express-blog');
 
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+    console.log(username);
+    console.log(password);
+
+    if (username === "Yukio" || username === "admin") {
+      return done(null, username.toString() );
+    }
+    else {
+      return done(null, false, {message: 'FAILLLLLL'});
+    }
+  }
+));
+
+passport.serializeUser(function(user, done) {
+  done(null, user);
+})
+
+passport.deserializeUser(function(user, done) {
+  done(null, user)
+})
+
 function ensureAuthenticated(req, res, next) {
-  if (res.isAuthenticated()) { return next(); }
+  // if (res.isAuthenticated()) { return next(); }
+  if (req.user) { return next(); }
+
+  console.log(req.user);
   res.redirect('/login');
 }
 
-router.get('/login', function(res, req) {
+router.get('/login', function(req, res) {
   res.render('login');
 });
 
-router.post('/login', function(res, req) {
-
-});
+router.post('/login',
+  passport.authenticate('local', { successRedirect: '/',
+                                   failureRedirect: '/login' })
+);
 
 router.authenticate = passport.authenticate('local', {
   successRedirect: 'blog/admin',
   failureRedirect: '/login'
 });
 
-router.get('/', function (req, res) {
+router.get('/', ensureAuthenticated, function (req, res) {
+  console.log('hi');
+
   Blog.find( function(err, blogs) {
     if (err) throw err;
 
--- app/server.js
+++ app/server.js
@@ -6,6 +6,17 @@ var bodyParser = require('body-parser');
 var livereload = require('connect-livereload');
 var livereloadport = 35729;
 var methodOverride = require('method-override');
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+var auth = require('./controllers/auth');
+
+// app.use(express.bodyParser());
+app.use(session({ secret: 'keyboard cat',   resave: false,
+  saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.set('views', path.join(__dirname, 'views'));
 app.use(livereload({port: livereloadport}));
@@ -14,6 +25,7 @@ app.use(bodyParser.json());
 app.set('view engine', 'jade');
 app.use(methodOverride('_method'));
 app.use(express.static('public/'));
+app.use(cookieParser());
 
 app.use('/', router);
 
